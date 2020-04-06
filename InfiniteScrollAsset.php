62fbf621d94bb75fe0faecf792dda6f8aac50182
Katerina <burinal@yandex.ru>
2020-04-07 02:39:38 +0300

    Create InfiniteScrollAsset.php

---
+++ InfiniteScrollAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace burinal\infscroll;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class InfiniteScrollAsset
+ * @package burinal\infscroll
+ */
+class InfiniteScrollAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/webcreate/jquery-ias/src';
+
+    public $js = [
+        'callbacks.js',
+        'jquery-ias.js',
+        'extension/history.js',
+        'extension/noneleft.js',
+        'extension/paging.js',
+        'extension/spinner.js',
+        'extension/trigger.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
